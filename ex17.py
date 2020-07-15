7589f7fe387baf0d47877cd2da95db954755f42e
valerie.ama.ogoe@gmail.com <Ogoe_v@soshgic.edu.gh>
2020-07-15 22:15:57 +0000

    More Files

--- ex17.py
+++ ex17.py
@@ -6,12 +6,12 @@ script, from_file, to_file = argv
 print(f"Copying from {from_file} to {to_file}")
 
 # we could do these two on one line, how?
-in_file = open(from_line)
+in_file = open(from_file)
 indata = in_file.read()
 
 print(f"The input file is {len(indata)} bytes long")
 
-print(f"Does the output file exist?" {exists(to_file)})
+print(f"Does the output file exist? {exists(to_file)}")
 print("Ready, hit RETURN to continue, CTRL-C to abort.")
 input ()
 
