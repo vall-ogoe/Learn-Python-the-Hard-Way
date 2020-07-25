fae235527ea2517156b2aa8a79afcd9acb38e0e1
valerie.ama.ogoe@gmail.com <Ogoe_v@soshgic.edu.gh>
2020-07-25 22:22:56 +0000

    Learning to Speak Object-Oriented

--- ex36.py
+++ ex36.py
@@ -1,6 +1,25 @@
 from sys import exit
 
-print("Enter your chosen name: ")
-name = input("> ")
+class Character:
 
+    die = "Dead"
+
+    def __init__(self):
+        self.gender = gender
+        self.name = name
+        self.status = status
+        self.age = age
+
+    def kill_char(self):
+        self.status = self.die
+        return f"{self.name} is now dead."
+
+print("Enter your chosen name: ")
+username = input("> ")
+print("Enter your desired gender: ")
+playergender = input("> ")
+
+player = Character(playergender,username,"Alive","15")
+
+characters = [username, ]
 
